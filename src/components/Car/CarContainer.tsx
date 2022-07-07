03f822bb76b4d2029da0ec1e593c1e7ff0028999
Gilles Tanson <gillestanson@googlemail.com>
2022-07-07 23:29:54 +0200

    added husky hooks

--- src/components/Car/CarContainer.tsx
+++ src/components/Car/CarContainer.tsx
@@ -4,20 +4,20 @@ import React from "react"
 import { styled } from "@mui/material"
 
 const CarBox = styled("div")(() => ({
-	display: "flex",
-	flexDirection: "row",
-	border: "1px solid lightGrey",
-	borderRadius: "5px",
-	padding: "0.625rem",
-	color: "grey",
-	flexGrow: "1",
-	flewWrap: "wrap",
-	justifyContent: "center",
-	margin: "0 auto 1rem auto",
-	minWidth: "250px",
-	maxWidth: "850px",
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid lightGrey",
+    borderRadius: "5px",
+    padding: "0.625rem",
+    color: "grey",
+    flexGrow: "1",
+    flewWrap: "wrap",
+    justifyContent: "center",
+    margin: "0 auto 1rem auto",
+    minWidth: "250px",
+    maxWidth: "850px",
 }))
 
 export default function CarContainer() {
-	return <CarBox>Hier könnte Ihr Car Model stehen!</CarBox>
+    return <CarBox>Hier könnte Ihr Car Model stehen!</CarBox>
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,14 @@
 import { createTheme } from "@mui/material/styles"
 
 const theme = createTheme({
-	// palette: {
-	// 	primary: {
-	// 		main: "#011936",
-	// 	},
-	// 	secondary: {
-	// 		main: "#FFDA1A",
-	// 	},
-	// }
+    // palette: {
+    // 	primary: {
+    // 		main: "#011936",
+    // 	},
+    // 	secondary: {
+    // 		main: "#FFDA1A",
+    // 	},
+    // }
 })
 
 export default theme
