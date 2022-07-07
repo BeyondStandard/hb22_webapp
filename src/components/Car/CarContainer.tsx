8068cdaed1a46da56824b7265c4e01ccbd4de4d2
Konstantin Seitz <konstantin.seitz@tchibo.de>
2022-07-08 00:02:38 +0200

    fix: fix double import

--- src/components/Car/CarContainer.tsx
+++ src/components/Car/CarContainer.tsx
@@ -28,7 +28,6 @@ export default function CarContainer() {
                 <Suspense fallback={null}>
                     <CarModel />
                     <OrbitControls />
-                    <Environment preset="city" background={false} />
                 </Suspense>
             </Canvas>
         </CarBox>
--- src/components/Car/CarModel.tsx
+++ src/components/Car/CarModel.tsx
@@ -1,6 +1,3 @@
-import React from "react"
-// import PropTypes from 'prop-types'
-
 import React, { useRef } from "react"
 import { useGLTF } from "@react-three/drei"
 
